c9b8cbee1c1efa640b8d83987b23970a21d6e596
Yuya Asanuma <yuya.asanuma@devenues.com>
2022-07-01 21:27:56 +0900

    よくわからんコミット

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const data = [
+  {
+    id: "001",
+    name: "フシギバナ",
+    type: "タイプ:くさ・どく",
+    feature: "特性：しんりょく",
+  },
+  {
+    id: "002",
+    name: "リザードン",
+    type: "タイプ：ほのお・ひこう",
+    feature: "特性：もうか",
+  },
+  {
+    id: "003",
+    name: "カメックス",
+    type: "タイプ：みず",
+    feature: "特性：げきりゅう",
+  },
+  {
+    id: "004",
+    name: "ピカチュウ",
+    type: "タイプ：でんき",
+    feature: "特性：せいでんき",
+  },
+  {
+    id: "005",
+    name: "カビゴン",
+    type: "タイプ：ノーマル",
+    feature: "特性：あついしぼう",
+  },
+]
+
+
+app.get('/pokemon', function (req, res) {
+  res.send(data)
+})
+
+app.listen(3000)
